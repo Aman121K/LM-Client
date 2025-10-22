f4da68fb9d50f0290622a475d17ef5ce237d3051
Vikas Tiwari <94979466+Aman121K@users.noreply.github.com>
2025-10-22 12:08:27 +0530

    Uncomment BASE_URL for development and production

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ const config = {
         // BASE_URL: 'http://localhost:5000/api',
         // https://lm-server-ladl.onrender.com/
         
-        //BASE_URL: 'https://lm-server-ladl.onrender.com/api' Working one
+        BASE_URL: 'https://lm-server-ladl.onrender.com/api'  //Working one
         // BASE_URL: 'https://lm-server-pmuo.onrender.com/api' Backup to tomorrow
     },
     production: {
-        // BASE_URL: 'https://lm-server-ladl.onrender.com/api' // Replace with your actual production URL new one
+         BASE_URL: 'https://lm-server-ladl.onrender.com/api' // Replace with your actual production URL new one
         // BASE_URL: 'https://lm-server-pmuo.onrender.com/api' Backup to tomorrow
     }
 };
